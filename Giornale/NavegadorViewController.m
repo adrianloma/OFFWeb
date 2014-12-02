95f445907ac35f0774586fa6887d157e9b032f8f
Adrian Lozano <adrian.loma@gmail.com>
2014-12-01 18:17:06 -0600

    adds duplicate detection when adding articles in navegadorViewController. adds function to singleton to check for duplicates: isAlreadyAdded

--- Giornale/NavegadorViewController.m
+++ Giornale/NavegadorViewController.m
@@ -75,39 +75,47 @@
 }
 
 - (IBAction)guardarArticulo:(id)sender {
-    //NSLog(@"hola en guardar");
     NSString *urlOriginal = self.vistaWeb.request.URL.absoluteString;
+    
     
     if (urlOriginal.length > 31){
     
+        //reformat url string
         NSString *titulo = [urlOriginal substringFromIndex:31];
         titulo = [titulo capitalizedString]; //capitaliza titulo
         titulo = [titulo stringByReplacingOccurrencesOfString:@"_And_" withString:@"_and_"];
         titulo = [titulo stringByReplacingOccurrencesOfString:@"_Of_" withString:@"_of_"];
         titulo = [titulo stringByReplacingOccurrencesOfString:@"_The_" withString:@"_the_"];
         NSString *url = [[NSString alloc] initWithFormat:@"http://en.wikipedia.org/w/index.php?title=%@&action=raw", titulo];
-        NSLog(url);
         titulo = [titulo stringByReplacingOccurrencesOfString:@"_" withString:@" "];
         NSURL *urlRequest = [NSURL URLWithString:url];
         
-        NSString *html = [NSString stringWithContentsOfURL:urlRequest encoding:NSUTF8StringEncoding error:nil];
-        NSLog(html);
         
-        if (!html){
-            html = [[NSString alloc] initWithFormat:
-                    @"<html><h1>Error en documento. Favor de revisar el articulo en linea.</h1></br>:'(</html>"];
+        //check for duplicates
+        if ([_singleArticulos isAlreadySavedWithURL:urlOriginal]) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Duplicate!" message:@"The page you're trying to save is already in your saved articles!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+            
+        } else{ //not a duplicate
+            NSString *html = [NSString stringWithContentsOfURL:urlRequest encoding:NSUTF8StringEncoding error:nil];
+            //NSLog(html);
+            
+            if (!html){
+                html = [[NSString alloc] initWithFormat:
+                        @"<html><h1>Error en documento. Favor de revisar el articulo en linea.</h1></br>:'(</html>"];
+            }
+            
+            NSDictionary *artPorAgregar = [NSDictionary dictionaryWithObjectsAndKeys:
+                                           titulo,@"titulo",
+                                           urlOriginal,@"url",
+                                           html,@"contenido",
+                                           nil];
+            
+            [self.singleArticulos agregarArticulo:artPorAgregar];
+            
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[artPorAgregar objectForKey:@"titulo"] message:@"Your article has been added to your news." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
         }
-        
-        NSDictionary *artPorAgregar = [NSDictionary dictionaryWithObjectsAndKeys:
-                                       titulo,@"titulo",
-                                       urlOriginal,@"url",
-                                       html,@"contenido",
-                                       nil];
-        
-        [self.singleArticulos agregarArticulo:artPorAgregar];
-        
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[artPorAgregar objectForKey:@"titulo"] message:@"Your article has been added to your news." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        [alertView show];
     
     } else {
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"URL error" message:@"The page you're trying to save is not an article. \nPage not saved." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
